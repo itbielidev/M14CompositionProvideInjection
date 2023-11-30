80b7cd154093364f2af3868286158f5a7ed5b0a3
carol b <carolina.blanco.7e6@itb.cat>
2023-11-30 17:42:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Order from '@/components/Order.vue'
+</script>
+
+<template>
+<h1>The Snazzy Burger</h1>
+
+</template>
+
+<style>
+
+h1{
+  text-align: center;
+}
+</style>
